bd68dd4b02419c4b334a16df56c650025be3d2b9
Andrew Miracle <me@andrewmiracle.com>
2024-03-27 18:58:45 +0000

    implement substack embded

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import { Saira, Fraunces } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/react";
+import Script from "next/script";
 
 const inter = Saira({
   subsets: ["latin"],
@@ -31,6 +32,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans ${inter.variable} ${fraunces.variable}`}>
         <TRPCReactProvider>{children}</TRPCReactProvider>
+        <Script src="https://substackapi.com/widget.js" async />
+        <Script src="https://substackapi.com/embeds/feed.js" async />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
 "use client";
 
 import { Disclosure } from "@headlessui/react";
-import {
-  ArrowPathIcon,
-  CloudArrowUpIcon,
-  Cog6ToothIcon,
-  FingerPrintIcon,
-  LockClosedIcon,
-  ServerIcon,
-} from "@heroicons/react/20/solid";
 
 const faqs = [
   {
@@ -49,10 +41,55 @@ export default function Example() {
   return (
     <div className="bg-white">
       {/* Header */}
-      <LandingHero />
+      <header>
+        <LandingHero />
+      </header>
 
       <main>
-        {/* Hero section */}
+        {/* Newletter section */}
+        <div className="relative isolate mt-32 bg-white px-6 sm:mt-56 lg:px-8">
+          <div
+            className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl"
+            aria-hidden="true"
+          >
+            <div
+              className="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+            />
+          </div>
+
+          <div className="mx-auto max-w-2xl text-center lg:max-w-4xl">
+            <h2 className="text-base font-semibold leading-7 text-indigo-600">
+              Newletter
+            </h2>
+            <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+              The right price for you, whoever you are
+            </p>
+          </div>
+
+          <div id="substack-feed-embed"></div>
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                        window.SubstackFeedWidget = {
+                          substackUrl: "aifoundry.substack.com",
+                          posts: 3,
+                          colors: {
+                            primary: "#000000",
+                            secondary: "#3C3737",
+                            background: "#ECECEC",
+                          }
+                        };
+
+                      `,
+            }}
+          />
+        </div>
+        {/* End Newletter section */}
 
         {/* Social Proof parallax image */}
         <SocialProofHero />
@@ -96,18 +133,23 @@ function LandingHero() {
               fugiat aliqua.
             </p>
             <div className="mt-10 flex items-center gap-x-6">
-              <a
-                href="#"
-                className=" bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                Learn more <span aria-hidden="true">→</span>
-              </a>
+              <div id="custom-substack-embed"></div>
+
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+              window.CustomSubstackWidget = {
+                substackUrl: "prostack.substack.com",
+                placeholder: "example@gmail.com",
+                buttonText: "Subscribe",
+                theme: "orange",
+
+
+              };
+
+              `,
+                }}
+              />
             </div>
           </div>
         </div>
